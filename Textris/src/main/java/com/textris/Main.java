9711d403ed9917abc1cbe4898ab20aaff55ef57e
crs1603 <crs1603@uah.edu>
2025-12-01 19:14:08 -0600

    Mybad lol I duped Main (Fixed)

--- Textris/src/main/java/com/textris/Main.java
+++ Textris/src/main/java/com/textris/Main.java
@@ -13,29 +13,6 @@ import com.textris.ui.MainMenuUI;
  * - Pretty much everything
  */
 public class Main {
-    public static void main(String[] args) throws InterruptedException{
-        System.out.println("Launching textris game");
-
-        Application.launch(MainMenuUI.class); // Launch JavaFX UI
-
-        System.out.println("Exiting textris game.");
-    }
-}
-package com.textris;
-
-import javafx.application.Application;
-import com.textris.ui.MainMenuUI;
-
-/**
- * This class starts the program.
- *
- * Responsibilities:
- * - Runs the program
- *
- * Collaborators:
- * - Pretty much everything
- */
-public class Main {
     /**
     * The main function.
     * 
